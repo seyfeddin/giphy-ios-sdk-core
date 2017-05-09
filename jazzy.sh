c7fad2090ad686b6097e504fecb47029e1b83153
Cem Kozinoglu <cem@tapslash.com>
2017-05-09 14:32:37 -0400

    Jazzy documentation and Shell script to run documentation (#11)
    * Jazzy documentation and Shell script to run documentation
    
    * license

---
+++ jazzy.sh
@@ -0,0 +1,7 @@
+jazzy \
+  --clean \
+  --author Giphy \
+  --author_url https://www.giphy.com \
+  --github_url https://github.com/Giphy/giphy-ios-sdk-core \
+  --module-version 1.0.0 \
+  --module GiphyCoreSDK
