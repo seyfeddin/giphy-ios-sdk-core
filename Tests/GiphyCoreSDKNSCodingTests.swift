ea914aafea75a6a68a084ed99ca4402f3333c440
Cem Kozinoglu <cem@tapslash.com>
2017-09-27 12:24:01 -0400

    MPLV2

--- Tests/GiphyCoreSDKNSCodingTests.swift
+++ Tests/GiphyCoreSDKNSCodingTests.swift
@@ -2,8 +2,12 @@
 //  GiphyCoreSDKNSCodingTests.swift
 //  GiphyCoreSDK
 //
-//  Created by Cem Kozinoglu, Gene Goykhman on 5/23/17.
+//  Created by Cem Kozinoglu, Gene Goykhman on 4/24/17.
 //  Copyright © 2017 Giphy. All rights reserved.
+//
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
 import XCTest
