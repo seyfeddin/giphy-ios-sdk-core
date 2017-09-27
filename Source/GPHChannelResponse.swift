cf0793f243e1d50cf2af4db0ead60848df6def3f
Cem Kozinoglu <cem@tapslash.com>
2017-09-27 12:01:06 -0400

    License update

--- Source/GPHChannelResponse.swift
+++ Source/GPHChannelResponse.swift
@@ -2,8 +2,12 @@
 //  GPHChannelResponse.swift
 //  GiphyCoreSDK
 //
-//  Created by David Hargat on 9/11/17.
+//  Created by Cem Kozinoglu, Gene Goykhman, David Hargat on 4/24/17.
 //  Copyright © 2017 Giphy. All rights reserved.
+//
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
 import Foundation
