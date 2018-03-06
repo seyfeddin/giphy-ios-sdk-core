59029b42a9ea6a7489cadecf4e79532ccfc678ef
dustfire <cem@giphy.com>
2018-03-06 16:15:17 -0500

    properties and funcs for Stateful request

--- Source/GPHStatefulRequest.swift
+++ Source/GPHStatefulRequest.swift
@@ -20,8 +20,30 @@ class GPHStatefulRequest: GPHRequest {
     var retryTimer:Timer? = nil
     var hasRequestInFlight: Bool = false
     
+    // This flag isn't set until we've receive at least a first response -
+    // even if it was empty.
+    var hasReceivedAResponse: Bool = false
+    
+    // This flag is set IFF we have received a failure more recently
+    // than a response.
+    var hasReceivedAFailure: Bool = false
+    
+    // This flag is set IFF we have received an empty response, which
+    // should indicate that we have "bottomed out" in the paging.
+    var hasReceivedEmptyResponse: Bool = false
+    
+    // More reliable way of determining whether the query has returned all paginated results
+    var hasAlreadyReturnedAllResults: Bool = false
+
+    
+    // Initiate a new request if necessary.
+    //
+    // 1) If force is YES, always create a new request.
+    // 2) If force is NO, do not create a new request if there is already a pending retry.
     func newRequest(force: Bool) {}
-    func resetRequest() {}
+    
+    
+    func resetRequest(fireEventImmediately: Bool) {}
     func retryRequest() {}
     func cancelRequest() {}
     func cancelRetry() {}
